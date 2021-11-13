fb7ad2ff1e925deed9119c7a30ec96bd1c0d2c4d
samarthlawania <93425520+samarthlawania@users.noreply.github.com>
2021-11-13 12:50:38 +0530

    Add files via upload

---
+++ remaning day in year 2019.py
@@ -0,0 +1,5 @@
+D = int(input("Enter the date of present day"))
+M = int(input("Enter the present month"))
+#No._of_days_in_year_2019 == 365
+R = 365 - (D + (M - 1))
+print(f"The number of remaning days in year 2019 is {R}") 
